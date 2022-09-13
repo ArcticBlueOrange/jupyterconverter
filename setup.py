e00e2d48248bb8bee88164787b655460a124d8ec
ArcticBlueOrange <brando1411@gmail.com>
2022-09-13 09:54:59 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='j2py',
-    version='0.3.3.1',
+    version='0.3.3.5',
     license='gpl-3.0',
     author="Alberto Brandolini",
     author_email='brando1411@gmail.com',
